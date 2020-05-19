c4444f666278bd8f01ee90da0d8ebc970c5cd671
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-19 08:46:43 +0200

    working with components

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: Book[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.books = [{
+      isbn: '111',
+      title: 'Angular',
+      description: 'Tolles Buch',
+      rating: 5
+    },
+    {
+      isbn: '222',
+      title: 'AngularJS',
+      description: 'Altes Buch',
+      rating: 3
+    },
+    {
+      isbn: '333',
+      title: 'React',
+      description: 'Anderes Buch',
+      rating: 1
+    },
+  ];
+  }
+}
