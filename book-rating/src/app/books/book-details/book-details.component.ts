5ac2aa831204755ad54019673854b17466b0a28b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-28 12:51:28 +0200

    rxjs: antipattern calling an cold observable multiple times

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,27 +1,20 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, timer, Subscription, Observable } from 'rxjs';
-import { map, tap, filter, scan, reduce, mergeMap, concatMap, switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+
 import { BookStoreService } from '../shared/book-store.service';
-import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  isbn: string;
-  book: Book;
+  book$ = this.route.paramMap.pipe(
+    map(p => p.get('isbn')),
+    switchMap(isbn => this.bs.getSingleBook(isbn))
+  );
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
-
-  ngOnInit(): void {
-    this.route.paramMap.pipe(
-      map(p => p.get('isbn')),
-      switchMap(isbn => this.bs.getSingleBook(isbn))
-    ).subscribe(book => this.book = book);
-
-  }
 }
