ad989bd547f867e291710629d857b59a1100264c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-29 13:21:57 +0200

    ngrx: effects

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookStoreService } from '../shared/book-store.service';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { loadBooks } from '../store/book.actions';
+import { selectBooks, selectBooksLoading, selectBookViaIsbn } from '../store/book.selectors';
 
 /**
  * Dieser Text ist später in der Dokumentation zu sehen:
@@ -19,7 +20,11 @@ import { loadBooks } from '../store/book.actions';
 })
 export class DashboardComponent {
 
-  books: Book[] = [];
+  books$ = this.store.pipe(select(selectBooks));
+  loading$ = this.store.pipe(select(selectBooksLoading));
+
+  book42$ = this.store.pipe(select(selectBookViaIsbn, { isbn: 42 }));
+
   currentDate: Date;
 
   constructor(private store: Store) {
