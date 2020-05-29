da2db5f283252dcb09024648ed7f38bf1bb17c14
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-29 12:51:21 +0200

    ngrx: selectors

--- book-rating/src/app/books/store/book.selectors.ts
+++ book-rating/src/app/books/store/book.selectors.ts
@@ -4,3 +4,21 @@ import * as fromBook from './book.reducer';
 export const selectBookState = createFeatureSelector<fromBook.State>(
   fromBook.bookFeatureKey
 );
+
+export const selectBooksLoading = createSelector(
+  selectBookState,
+  state => state.loading
+);
+
+export const selectBooks = createSelector(
+  selectBookState,
+  state => state.books
+);
+
+
+// just an example -- computed selector
+
+export const selectBookViaIsbn = createSelector(
+  selectBooks,
+  (state, props) => state.find(b => b.isbn === props.isbn)
+);
