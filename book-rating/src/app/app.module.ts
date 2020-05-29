066af18944a2661f0d4a4f6b1db1304ac1ac3988
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-29 11:14:41 +0200

    ng g feature books/store/book --module books/books --api --defaults

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BooksModule } from './books/books.module';
 import localeDe from '@angular/common/locales/de';
 import localeEn from '@angular/common/locales/en';
 import { registerLocaleData } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,10 @@ import { registerLocaleData } from '@angular/common';
     AppRoutingModule,
     BooksModule,
     // AppRoutingEndModule
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'de' }
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -8,6 +8,10 @@ import { BookComponent } from './book/book.component';
 import { ToArrayPipe } from './shared/to-array.pipe';
 import { CreateBookComponent } from './create-book/create-book.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { StoreModule } from '@ngrx/store';
+import * as fromBook from './store/book.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './store/book.effects';
 
 
 @NgModule({
@@ -22,6 +26,8 @@ import { BookDetailsComponent } from './book-details/book-details.component';
     CommonModule,
     BooksRoutingModule,
     ReactiveFormsModule,
+    StoreModule.forFeature(fromBook.bookFeatureKey, fromBook.reducer),
+    EffectsModule.forFeature([BookEffects]),
     // HttpClientModule --- NIIIEMALS machen! https://github.com/angular/angular/issues/20575
   ],
   exports: [
