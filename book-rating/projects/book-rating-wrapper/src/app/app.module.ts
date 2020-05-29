26d7ae84fba73bf333e6358814e8563fdeab8379
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-29 10:46:28 +0200

    angular elements

--- book-rating/projects/book-rating-wrapper/src/app/app.module.ts
+++ book-rating/projects/book-rating-wrapper/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, DoBootstrap, Injector } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { BookComponent } from 'src/app/books/book/book.component';
+import { ToArrayPipe } from 'src/app/books/shared/to-array.pipe';
+import { createCustomElement } from '@angular/elements';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent
+    BookComponent,
+    ToArrayPipe
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  // entryComponents --- bis Angular 9
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const webComponent = createCustomElement(BookComponent, { injector: this.injector });
+    // <book-component>
+    customElements.define('book-component', webComponent);
+  }
+}
