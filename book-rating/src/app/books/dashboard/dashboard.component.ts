781c9b3c3c389cf80b0d9070560cc7053879e837
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-29 11:36:59 +0200

    ngrx: dispatch actions

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookStoreService } from '../shared/book-store.service';
+import { Store } from '@ngrx/store';
+import { loadBooks } from '../store/book.actions';
 
 /**
  * Dieser Text ist später in der Dokumentation zu sehen:
@@ -15,40 +17,42 @@ import { BookStoreService } from '../shared/book-store.service';
   // VORSICHT: Bug, sobald wir Daten über Ajax laden
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
   books: Book[] = [];
   currentDate: Date;
 
-  constructor(private br: BookRatingService, private bs: BookStoreService) {
+  constructor(private store: Store) {
     this.currentDate = new Date();
-  }
 
-  ngOnInit(): void {
-    this.bs.getBooks().subscribe(books => this.books = books);
+    // dispatch Action!
+    this.store.dispatch(loadBooks());
   }
 
   doRateDown(book: Book) {
-    const ratedBook = this.br.rateDown(book);
-    this.update(ratedBook);
+    // TODO:
+    // this.store.dispatch(rateUp());
+
+    // const ratedBook = this.br.rateDown(book);
+    // this.update(ratedBook);
   }
 
   doRateUp(book: Book) {
-    const ratedBook = this.br.rateUp(book);
-    // const ratedBook = {
-    //   ...book,
-    //   rating: 5
-    // };
-    this.update(ratedBook);
+    // const ratedBook = this.br.rateUp(book);
+    // // const ratedBook = {
+    // //   ...book,
+    // //   rating: 5
+    // // };
+    // this.update(ratedBook);
   }
 
   update(ratedBook: Book) {
-    this.books = this.books
-      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
-      .sort((a, b) => b.rating - a.rating);
+    // this.books = this.books
+    //   .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+    //   .sort((a, b) => b.rating - a.rating);
   }
 
   addBook(book: Book) {
-    this.books = [...this.books, book];
+    // this.books = [...this.books, book];
   }
 }
